a91fa384b517183c75e54c4d1c51022b3d077946
Gonzalo Ezequiel <gep.perez@gmail.com>
2022-06-05 05:20:20 -0300

    Tp3 back

---
+++ RecuperatoriosTP/TP_2/TP-02/Entidades/Sedan.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        public enum ETipo { CuatroPuertas, CincoPuertas }
+        ETipo tipo;
+
+        #region CONSTRUCTORES
+        
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            tipo = ETipo.CuatroPuertas;
+        }
+
+        /// <summary>
+        /// Constructor SEDAN con tipo especifico
+        /// Constructor SEDAN, toma la referencia del constructor Sedan(EMarca marca, string chasis, ConsoleColor color)
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        /// <param name="tipo"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo) : this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Sedan son 'Mediano'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        /// <summary>
+        /// Muestra los datos de un Vehiculo SEDAN
+        /// </summary>
+        /// <returns>string Retorna el resultado del sb mostrando los datos del SEDAN</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP_2/TP-02/Entidades/Vehiculo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
+        }
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+
+        protected EMarca marca;
+        protected string chasis;
+        protected ConsoleColor color;
+
+        protected Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+        
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        protected abstract ETamanio Tamanio { get; }
+
+        #region SOBRECARGAS
+
+        /// <summary>
+        /// "Castea a string" todos los detos de un vehiculo
+        /// </summary>
+        /// <param name="p">string Retorna el resultado del sb segun chasis, marca, color, (separacion) de un vehiculo</param>
+        public static explicit operator string(Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("CHASIS: {0}\r\n", p.chasis);
+            sb.AppendFormat("MARCA : {0}\r\n", p.marca.ToString());
+            sb.AppendFormat("COLOR : {0}\r\n", p.color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1">Primer Vehículo</param>
+        /// <param name="v2">Segundo Vehículo</param>
+        /// <returns>True si los dos Vehículos son iguales, false si no lo son</returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return v1.chasis == v2.chasis;
+        }
+
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1">Primer Vehículo</param>
+        /// <param name="v2">Segundo Vehículo</param>
+        /// <returns>True si los dos Vehículos son distintos, false si son iguales</returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1.chasis == v2.chasis);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            StringBuilder retorno = new StringBuilder();
+
+            retorno.AppendLine(this.GetType().Name.ToUpper());
+            retorno.AppendLine((string)this);
+            retorno.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            retorno.AppendLine();
+            retorno.AppendLine("---------------------");
+
+            return retorno.ToString();
+        }        
+    }
+}
